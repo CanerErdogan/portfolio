312575a9d77adb30c719bd7b65a6ef04db47ee85
Caner <caner.erdogan.92@gmail.com>
2020-10-28 22:52:38 +0300

    Background and font enhancements

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const pages = document.querySelectorAll(".page");
 
+
+
 pages.forEach(page => {
     page.addEventListener("click", e => {
         pages.forEach(p => {
             if (p !== page){
                 p.classList.remove("expanded");
-                // page.getElementsByClassName("page-content")[0].style.marginLeft = "-100vw"
-                p.getElementsByClassName("page-content")[0].style.display = "none";
+                let content = p.getElementsByClassName("page-content");
+                if (content.length) {
+                    content[0].style.display = "none";
+                };
             }
         });
         page.classList.add("expanded");
-        setTimeout(x => {page.getElementsByClassName("page-content")[0].style.display = "unset";}, 500);
+        let content = page.getElementsByClassName("page-content");
+        if (content.length) {
+            setTimeout(x => {content[0].style.display = "unset";}, 250);
+        }
     });
 });
 
